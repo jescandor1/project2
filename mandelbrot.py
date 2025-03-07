57ca8a9595276f03b40a4424810ada284e442f0e
jescandor1 <jescandor1@ufl.edu>
2025-03-07 12:18:02 -0500

    set up mandelbrot.py

---
+++ mandelbrot.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def get_escape_time(c: complex, max_iterations: int) -> int | None:
+    ...
+
+def get_complex_grid(top_left: complex, bottom_right: complex, step: float) -> np.ndarray:
+    ...
+
+def get_escape_time_color_arr(c_arr: np.ndarray, max_iterations: int) -> np.ndarray:
+    ...
+
+def get_julia_color_arr() -> ... :
+    ...
